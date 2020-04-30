ca82fd5b6068c22ec8acdc8e80adb6fe87e5d899
anu <anujp.jp@gmail.com>
2020-04-30 12:00:01 -0700

    add checkout

--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core'
+import { NgForm } from '@angular/forms'
+import { OrderRepository } from '../model/order.repository'
+import { Order } from '../model/order.model'
 
-@Component({template:`<h3>CheckoutComponent</h3>`})
+@Component({
+  templateUrl: 'checkout.component.html',
+  styleUrls: ['checkout.component.css'],
+})
+export class CheckoutComponent {
+  orderSent: boolean = false
+  submitted: boolean = false
 
-export class CheckoutComponent {}
+  constructor(public repository: OrderRepository, public order: Order) {}
+
+  submitOrder(form: NgForm) {
+    this.submitted = true
+    if (form.valid) {
+      this.repository.saveOrder(this.order).subscribe((order) => {
+        this.order.clear()
+        this.orderSent = true
+        this.submitted = false
+      })
+    }
+  }
+}
