59e7bc7e87870a8105fe6412d4314d3c8fc21d03
anu <anujp.jp@gmail.com>
2020-04-29 17:05:46 -0700

    add cart summary

---
+++ src/app/store/cartSummary.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+import { Cart } from '../model/cart.model'
+@Component({
+
+  selector: 'cart-summary',
+
+  templateUrl: 'cartSummary.component.html',
+
+})
+export class CartSummaryComponent {
+
+  // the cart is injected by Angular
+
+  constructor(public cart: Cart) {}
+
+}
+
+
+ 
